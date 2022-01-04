9f72256b78a5be987f211c057a6a2a4f0630ffab
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-04 17:17:24 +0300

    Rewrote emloyeesAddForm in class style

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,35 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Добавьте нового сотрудника</h3>
-			<form
-				className="add-form d-flex">
-				<input type="text"
-					className="form-control new-post-label"
-					placeholder="Как его зовут?" />
-				<input type="number"
-					className="form-control new-post-label"
-					placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			employeesName: '',
+			wage: 0
+		}
+	}
 
-				<button type="submit"
-					className="btn btn-outline-light">Добавить</button>
-			</form>
-		</div>
-	)
+	render() {
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form
+					className="add-form d-flex">
+					<input type="text"
+						className="form-control new-post-label"
+						placeholder="Как его зовут?" />
+					<input type="number"
+						className="form-control new-post-label"
+						placeholder="З/П в $?" />
+
+					<button type="submit"
+						className="btn btn-outline-light">Добавить</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default EmployeesAddForm;
