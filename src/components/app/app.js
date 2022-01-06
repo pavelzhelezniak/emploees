5f0921f00ca6473d7bdd858976af7e6c54b23d9f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-06 12:06:33 +0300

    Made the button for deleting employees active and began to rewrite the App in a class style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel'
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,34 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ employeesName: "Alex Spiridovich", wage: 700, increase: false, id: 1 },
+				{ employeesName: "Pavel Zhaliazniak", wage: 1100, increase: true, id: 2 },
+				{ employeesName: "Dmitry Pudau", wage: 900, increase: false, id: 3 }
+			]
+		}
+	}
 
-	const data = [
-		{ employeesName: "Alex Spiridovich", wage: 700, increase: false, id: 1 },
-		{ employeesName: "Pavel Zhaliazniak", wage: 1100, increase: true, id: 2 },
-		{ employeesName: "Dmitry Pudau", wage: 900, increase: false, id: 3 }
-	]
+	render() {
+		return (
+			<div className="app" >
+				<AppInfo />
 
-	return (
-		<div className="app">
-			<AppInfo />
-
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
-			</div>
-			<EmployeesList data={data} />
-			<EmployeesAddForm />
-		</div >
-	);
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+				<EmployeesList
+					data={this.state.data}
+					onDelete={id => console.log(id)} />
+				<EmployeesAddForm />
+			</div >
+		);
+	}
 }
 
 export default App;
